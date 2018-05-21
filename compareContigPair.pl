7945f74babd19810b9111555c08a30c2cdea8175
Diego M Riano-Pachon <diriano@gmail.com>
2018-05-20 23:49:42 -0300

    Producing LASTZ alignment file in general format, with a defined list of fields, that will be used for filtering later on.

--- compareContigPair.pl
+++ compareContigPair.pl
@@ -17,6 +17,7 @@ while(<PAIRS>){
  next if /^Contig1\tContig2/;
  my ($contig1,$contig2,$len1,$len2,$sharedGenes,$geneIDs)=split(/\t/);
  my $outAlgn=$contig1."__".$contig2."_lastz.rdotplot";
+ my $outAlgnGeneral=$contig1."__".$contig2."_lastz.general";
  my $contig1File=$contig1.'.fasta';
  my $contig2File=$contig2.'.fasta';
  if(!-s $contig1File){
@@ -35,14 +36,21 @@ while(<PAIRS>){
  }
  my $outGenesContig1=$contig1.".genePos.txt";
  my $outGenesContig2=$contig2.".genePos.txt";
+ my $lastzJobID4R="LASTZ_".$contig1."_vs_".$contig2."_4R_".$$;
+ my $rscriptJobID="RSCRIPT_".$contig1."_vs_".$contig2."_".$$;
+ my $lastzJobID="LASTZ_".$contig1."_vs_".$contig2."_".$$;
  system("grep $contig1 $genePosFile > $outGenesContig1");
  system("grep $contig2 $genePosFile > $outGenesContig2");
- system("lastz $contig1File $contig2File --notransition --step=20 --gapped --format=rdotplot > $outAlgn");
- system("Rscript --vanilla /data1/bioinfo/HomolContigsByAnnotation/compareContigPairPlotAlignment.Rscript $outAlgn $outGenesContig1 $outGenesContig2");
- unlink $contig1File;
- unlink $contig2File;
- unlink $outAlgn;
- unlink $outGenesContig1;
- unlink $outGenesContig2;
+ my $cmd_lastz_general="module load lastz; lastz $contig1File $contig2File --notransition --step=20 --gapped --format=general:score,name1,strand1,size1,zstart1,end1,name2,strand2,size2,zstart2,end2,nmatch,nmismatch,ngap,continuity,identity,coverage > $outAlgnGeneral";
+ my $cmd_lastz_rdotplot="module load lastz ; lastz $contig1File $contig2File --notransition --step=20 --gapped --format=rdotplot > $outAlgn";
+ my $cmd_Rscript="module load R/3.4.1 ; Rscript --vanilla /data1/bioinfo/HomolContigsByAnnotation/compareContigPairPlotAlignment.Rscript $outAlgn $outGenesContig1 $outGenesContig2";
+ system("echo \"$cmd_lastz_general\" |qsub -q all.q -cwd -N $lastzJobID");
+ system("echo \"$cmd_lastz_rdotplot\" |qsub -q all.q -cwd -N $lastzJobID4R"); 
+ system("echo \"$cmd_Rscript\" |qsub -q all.q -cwd -N $rscriptJobID -hold_jid $lastzJobID4R");
+# unlink $contig1File;
+# unlink $contig2File;
+# unlink $outAlgn;
+# unlink $outGenesContig1;
+# unlink $outGenesContig2;
 }
 close PAIRS;
